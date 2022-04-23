66328212d3a40e74fa9192dc566bf7c19849e33b
etm-193 <ejtm.193@gmail.com>
2022-04-23 11:08:00 -0500

    Created a new branch
    Things are starting to slowly pick up with the course I'm following. Next lesson should be a simple string length verification program. From an organization perspective, and as a practice exercise with git and repositories I decided to add a branch.

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Learning Python repository
 
 In this repository I'll share my progress of 100 days of coding python for a minimum of an hour. To keep this organized. I'll be using this README file to explain the flow of the lessons and each new file, line etc.
 
-1.  Python Syntax Lesson
+1.  Python Syntax Basics Lesson
+    - Basic print command
+    - Data Type Strings & string manipulation
+    -
 2.  My First Program: "BAND NAME GENERATOR"
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Hello World")
 # This is the iconic first step, by using the comand print ("") we have just printed a string. Now this syntax looks very similar to java. I guess later my hypothesis confirmed or not.
+
+print("")
+
+print("Learning Concatenation")
+
+print("concatenation is created using the the '+' sign")
+
+# print("e.g. print('Hello' + 'world')"\n"New lines can be created with baslash and n" ) this was my first attempt at using this method and i made the mistake of splitting the statement and inserting the command in between two separate statements (which I'm not sure could have even been called separate as there was no coma or other symbol) the correct way is as follows:
+
+print("e.g. print('Hello' + 'world')\nNew lines can be created with baslash and n" )
+
