ed8d617967af6ec6f863634b855a827122fb8eae
etm-193 <ejtm.193@gmail.com>
2022-04-23 15:16:43 -0500

    fixing some oopsies

--- day1py/main.py
+++ day1py/main.py
@@ -1,2 +1,8 @@
-# This main.py doc is actually a cheat sheet of the basics for Python!
+# Just learned that the program wont run from separate files like an html. Apparently everything goes through the main.py sheet.
 
+# This will be the band name generator program! (cheat sheet will be a separate file)
+
+print("Welcome to the Band Generator")
+
+print(input("What city did you grow up in?\n") + " " +
+      input("What is the name of your pet\n?"))
