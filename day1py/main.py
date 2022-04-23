a8e4366ff7ba182ae1bff7596813c303d2b66c6f
etm-193 <ejtm.193@gmail.com>
2022-04-23 15:25:39 -0500

    Update main.py

--- day1py/main.py
+++ day1py/main.py
@@ -2,7 +2,18 @@
 
 # This will be the band name generator program! (cheat sheet will be a separate file)
 
+# METHOD 1 (way i came up with)
+
 print("Welcome to the Band Generator")
+
 
 print(input("What city did you grow up in?\n") + " " +
       input("What is the name of your pet\n?"))
+
+# METHOD 2 (solution)
+
+print("Welcome to the Band Generator")
+
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
