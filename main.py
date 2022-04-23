d06fda4deb962f5b396eff4deaf306c12f78451c
etm-193 <ejtm.193@gmail.com>
2022-04-23 10:10:34 -0500

    main py doc
    my first py doc including my first impressions of py and the clasic "hello world"

---
+++ main.py
@@ -0,0 +1,2 @@
+print("Hello World")
+# This is the iconic first step, by using the comand print ("") we have just printed a string. Now this syntax looks very similar to java. I guess later my hypothesis confirmed or not.
