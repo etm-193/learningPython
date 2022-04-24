b24aaf0a692e952f3316d94357e88337170d581d
etm-193 <ejtm.193@gmail.com>
2022-04-24 01:52:19 -0500

    Math and functions
    diving into math and functions... my head hurts lol

--- day2/functions.py
+++ day2/functions.py
@@ -16,3 +16,28 @@ new_num_char = str(num_char)
 print = ("Your name has " + new_num_char + " characters.")
 
 # javaScript has slightly different syntax but the theory and application behind it is the same.
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(type(two_digit_number))
+
+digit_one = two_digit_number[0]
+
+digit_two = two_digit_number[1]
+
+
+new_digit_1 = int(digit_one)
+new_digit_2 = int(digit_two)
+
+output = new_digit_1 + new_digit_2
+
+print(output)
+
+
+# Took me a while to understand this exercise because i was trying to use the len command along with the int, in other words i was trying to skip a step apparently because i was casting at the same time i was getting the len this resulted in the digit being = 1 always. It was essential to understand that gathering the digit you want as a string using the variable[] first and then casing as a new variable = int(variableOld) to acheive this i should have done as such: digit_one = int(two_digit_number[0])
---
+++ day2/math.py
@@ -0,0 +1,18 @@
+# Mathematical operators
+
+from __future__ import division
+from doctest import Example
+
+
+addition = 3 + 2
+subtraction = 5 - 1
+multiplication = 9 * 2
+division = 4 / 2
+exponent = 2 ** 2
+
+
+# like manual math there are rules as to the order of math : pemdas -   PARENTHESIS, exponent, multiplication and division, addition and subtraction:
+
+Example = 3 * 3 + 3 / 3 - 3
+
+# left to right thats the way to go (program to see code execute step by step is called thony)
