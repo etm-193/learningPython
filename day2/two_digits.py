0b3d66b0ef6d1e2a7d51a0407ffb350a0f1734eb
Ernesto Torres <ejtm.193@gmail.com>
2023-05-05 05:00:18 +0000

    Pending changes exported from your codespace

---
+++ day2/two_digits.py
@@ -0,0 +1,12 @@
+#Two digit challenge
+
+# Putting casting data types and previous concepts into practice. Fun project for later automate
+# this program with a loop and insert an exception to prevent the user from not imputting any data.
+
+two_digit_number = input("Type a two digit number: ")
+
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+
+result = a + b
+print (result)
